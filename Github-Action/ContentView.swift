593c4dff6463e6938c11ca4555832ba6c70ee3c3
Navjot Singh Bedi <navjot.bedi@groundtruth.com>
2020-02-27 18:09:19 +0530

    Initial Commit

---
+++ Github-Action/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github-Action
+//
+//  Created by Navjot Bedi on 27/02/20.
+//  Copyright © 2020 Navjot Bedi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
