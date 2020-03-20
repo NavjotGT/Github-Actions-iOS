c2214e7bd3d5b2b3cee6d6bac5839be560e97b78
Navjot Singh Bedi <navjotsinghbedi@gmail.com>
2020-03-20 13:04:45 +0530

    Updated pipeline

---
+++ Github-Action/Pods/SDK/SDK/Calculate.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  SDK
+//
+//  Created by Navjot Bedi on 19/03/20.
+//  Copyright © 2020 Navjot Bedi. All rights reserved.
+//
+
+import Foundation
+
+public class Calculate {
+    
+    public init() {}
+    
+    public func add(x: Int, y: Int) -> Int{
+        return x+y
+    }
+    
+}
